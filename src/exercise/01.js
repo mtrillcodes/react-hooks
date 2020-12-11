c2c12c7470113247f712c7f23be0f73936be3c9c
Maxine Tillman <maxltillman@gmail.com>
2020-12-11 22:20:38 +0000

    task 1 extra credit complete

--- src/exercise/01.js
+++ src/exercise/01.js
@@ -5,7 +5,7 @@ import * as React from 'react'
 
 function Greeting() {
   // 💣 delete this variable declaration and replace it with a React.useState call
-  const [name, setName] = React.useState('')
+  const [name, setName] = React.useState('Max')
 
   function handleChange(event) {
     // 🐨 update the name here based on event.target.value
@@ -17,7 +17,7 @@ function Greeting() {
     <div>
       <form>
         <label htmlFor="name">Name: </label>
-        <input onChange={handleChange} id="name" />
+        <input onChange={handleChange} id="name" value={name} />
       </form>
       {name ? <strong>Hello {name}</strong> : 'Please type your name'}
     </div>
