6aebf67018e3ae0bab10f8f079115068516400dd
Maxine Tillman <maxltillman@gmail.com>
2020-12-12 12:23:51 +0000

    task 1 extra credit completed, more refined

--- src/exercise/01.js
+++ src/exercise/01.js
@@ -3,9 +3,9 @@
 
 import * as React from 'react'
 
-function Greeting() {
+function Greeting({initialName}) {
   // 💣 delete this variable declaration and replace it with a React.useState call
-  const [name, setName] = React.useState('Max')
+  const [name, setName] = React.useState(initialName)
 
   function handleChange(event) {
     // 🐨 update the name here based on event.target.value
@@ -25,7 +25,7 @@ function Greeting() {
 }
 
 function App() {
-  return <Greeting />
+  return <Greeting initialName="Max" />
 }
 
 export default App
